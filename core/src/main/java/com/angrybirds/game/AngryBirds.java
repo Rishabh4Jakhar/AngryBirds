bac7717df42b39a737630b776f26bf7b45134d80
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2024-10-25 19:07:21 +0530

    Optimized and add comments

--- core/src/main/java/com/angrybirds/game/AngryBirds.java
+++ core/src/main/java/com/angrybirds/game/AngryBirds.java
@@ -1,21 +1,21 @@
 package com.angrybirds.game;
 
 import com.angrybirds.game.Screens.PlayScreen;
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * Main game class for Angry Birds, extending the libGDX Game class.
+ * This class sets up the game window and initializes the first screen.
+ */
 public class AngryBirds extends Game {
+    // SpriteBatch used for drawing 2D textures
     public SpriteBatch batch;
+    // Virtual width and height of the game screen
     public static final int V_WIDTH = 1280;
     public static final int V_HEIGHT = 720;
 
-    // Dimensions of various sprites
+    // Dimensions of various sprites (commented out for now)
     /*
     public static final int RED_BIRD_WIDTH = 45;
     public static final int RED_BIRD_HEIGHT = 44;
@@ -26,6 +26,7 @@ public class AngryBirds extends Game {
     public static final int SLINGSHOT_WIDTH = 85;
     public static final int SLINGSHOT_HEIGHT = 200;
     */
+
     @Override
     public void create() {
         batch = new SpriteBatch();
